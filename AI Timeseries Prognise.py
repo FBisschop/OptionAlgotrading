25ecc33cad5d02f6f4e37f006538ddc7eea9355e
FBisschop <ef.roosegaardebisschop@gmail.com>
2024-03-07 14:33:03 +0100

    Inrichting OptionAlgotrading

---
+++ AI Timeseries Prognise.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import yfinance as yf
+import numpy as np
+import talib as ta
+import plotly.express as px
+from sklearn.metrics import classification_report
+from xgboost import XGBClassifier
+
+np.random.seed(42)
+
